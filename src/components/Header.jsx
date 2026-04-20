ef5e95ec0ae9985cbb2823df9adeaf6d3ac72f0d
Pranshu Sinha <pranshu.yashuuma@gmail.com>
2026-04-21 01:18:10 +0530

    improved lcp and lcs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Header.css";
 import { useTheme } from "../contexts/ThemeContext";
-import { useTranslation } from "react-i18next"; // <-- updated
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
   const { theme } = useTheme();
-  const { t } = useTranslation("header"); // <-- namespace for Header component
+  const { t } = useTranslation("header");
 
-  const words = t("words", { returnObjects: true }); // array of {text, color}
+  const words = t("words", { returnObjects: true }); 
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // @ts-ignore
       setIndex((prev) => (prev + 1) % words.length);
     }, 2000);
     return () => clearInterval(interval);
   }, [words]);
 
   return (
-    <div className={`hero-container ${theme}`}>
+    <header className={`hero-container ${theme}`}>
       <h1 className="hero-title">
         {t("titleStart")}{" "}
         <span
@@ -32,7 +31,7 @@ const Header = () => {
         {t("titleEnd")}
       </h1>
       <p className="hero-subtitle">{t("subtitle")}</p>
-    </div>
+    </header>
   );
 };
 
